fc5f990851c18893b2d708c1ba11c7aa644a3ced
MichaelTranDev <Tran.M183438@stu.sanjac.edu>
2025-11-10 23:47:40 -0600

    Update Superhero.h

--- src/Superhero.h
+++ src/Superhero.h
@@ -1,1 +1,25 @@
+#ifndef SUPERHERO_H
+#define SUPERHERO_H
+
+#include <string>
+using namespace std;
+
+// Struct to store superhero information
+struct Superhero {
+    string name;
+    string superpower;
+    int age;
+    int powerLevel;
+};
+
+// Function declarations
+void addHero(Superhero heroes[], int &count);
+void displayHero(Superhero heroes[], int count); // Display all heroes
+void displayHero(Superhero heroes[], int count, int index); // Overloaded: display by index
+void displayHero(Superhero heroes[], int count, string name); // Overloaded: display by name
+int searchHero(Superhero heroes[], int count, string name); // Returns index or -1
+void saveToFile(Superhero heroes[], int count, string filename);
+void loadFromFile(Superhero heroes[], int &count, string filename);
+
+#endif
 
